bc88094131cd319ca2f6032187c294da5a53572f
kimhyeonjoon <kimhyeomjoon01@gmail.com>
2024-04-18 16:55:02 +0900

    프로젝트 생성

---
+++ millie_test/millie_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  millie_test
+//
+//  Created by Hyeonjoon Kim_M1 on 2024/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
